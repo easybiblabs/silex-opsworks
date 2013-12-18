340f29bc1cfe55502dc8f5d4f83dd8ffccc0e137
Nils Adermann <naderman@naderman.de>
2013-12-18 21:31:13 +0100

    License Header

--- src/EasyBib/Service/Opsworks/OpsworksProvider.php
+++ src/EasyBib/Service/Opsworks/OpsworksProvider.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ * This file is part of easybib/opswork-provider
+ *
+ * (c) Imagine Easy Solutions, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author   Florian Holzhauer <fh-opsworks@fholzhauer.de>
+ * @license  BSD-2-Clause
+ * @link     http://www.imagineeasy.com
+ */
 namespace EasyBib\Service\Opsworks;
 
 use Silex\Application;
